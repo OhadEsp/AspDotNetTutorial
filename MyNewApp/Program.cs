44748f780a2f09a35cc29a6f708dab55ae94f644
Ohad Esperansa <ohad999@gmail.com>
2024-04-15 19:23:45 +0300

    Implementing a Web API in .NET [Pt 3]

--- MyNewApp/Program.cs
+++ MyNewApp/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos);
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var todo = todos.SingleOrDefault(t => t.Id == id);
+    return todo is not null 
+        ? TypedResults.Ok(todo) 
+        : TypedResults.NotFound();
+});
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
+
+app.MapDelete("/todos/{id}", (int id) =>
+{
+    var todo = todos.RemoveAll(t => t.Id == id);
+    return TypedResults.NoContent();
+});
 
 app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
